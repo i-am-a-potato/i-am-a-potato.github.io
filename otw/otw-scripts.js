3d33dc346b1deeb660f3f39cdc1c793b3300727f
i-am-a-potato <62171281+i-am-a-potato@users.noreply.github.com>
2022-09-28 19:50:58 +0800

    Add files via upload

---
+++ otw/otw-scripts.js
@@ -0,0 +1,21 @@
+//navigation button function
+
+  //to learning html page
+  function toHTML() {
+    document.location.href="../learningweb/learningweb.html";
+  }
+  
+  //to overthewire page
+  function toOTW() {
+    document.location.href="over.html";
+  }
+
+  //to pico page
+  function toPico() {
+    document.location.href="../pico/pico.html";
+  }
+
+  //to home page
+  function toHome() {
+    document.location.href="../home.html";
+  }
