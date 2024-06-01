b509f6caa66c70c0ae27b231f1e698594342b91d
i-am-a-potato <62171281+i-am-a-potato@users.noreply.github.com>
2024-06-01 11:51:23 +0800

    php files

---
+++ notHTML/js/scripts.js
@@ -0,0 +1,35 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// for overthewire content
+function openLevel(evt, level) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(level).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+}
+	// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
